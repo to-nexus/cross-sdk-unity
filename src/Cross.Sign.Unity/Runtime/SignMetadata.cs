13d2265a98ddb1734abcdf5fb97c2aea7b664896
Harvey <probe@to.nexus>
2025-05-30 18:28:30 +0900

    version up to 1.1.0

--- src/Cross.Sign.Unity/Runtime/SignMetadata.cs
+++ src/Cross.Sign.Unity/Runtime/SignMetadata.cs
@@ -5,6 +5,6 @@ namespace Cross.Sign.Unity
     public class SignMetadata
     {
         [VersionMarker]
-        public const string Version = "v1.3.0";
+        public const string Version = "v1.1.0";
     }
 }
