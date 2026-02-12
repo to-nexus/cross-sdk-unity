7dde194fa84c662c4adcca86fe184fa552f738d6
KyeongWonLee <return01010@gmail.com>
2026-02-12 17:18:16 +0900

    [fix] fix qrcode ui

--- sample/Cross.Sdk.Unity/Assets/Scripts/Dapp.cs
+++ sample/Cross.Sdk.Unity/Assets/Scripts/Dapp.cs
@@ -541,7 +541,7 @@ namespace Sample
 
             // Convert CAIP-2 chain reference to EIP-155 chain ID
             // This is equivalent to `account.ChainId.Split(":")[1]`, but allocates less memory
-            var ethChainId = Core.Utils.ExtractChainReference(account.ChainId);
+            var ethChainId = Cross.Core.Utils.ExtractChainReference(account.ChainId);
 
             typedData.Domain.ChainId = BigInteger.Parse(ethChainId);
             typedData.SetMessage(message);
